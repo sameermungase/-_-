2e715312797a83ca3d48c9e24015df3c0a1f6ef9
Sameer Mungase <143208446+sameermungase@users.noreply.github.com>
2024-07-01 19:15:47 +0530

    added 1st task of internship

---
+++ Student Grade Tracker/gradeTracker.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class gradeTracker {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Double> grades = new ArrayList<>();
+
+        System.out.println("Enter the number of students:");
+        int numStudents = scanner.nextInt();
+
+        for (int i = 0; i < numStudents; i++) {
+            System.out.println("Enter the grades for student " + (i + 1) + ":");
+            double grade = scanner.nextDouble();
+            grades.add(grade);
+        }
+
+        double sum = 0;
+        double highest = grades.get(0);
+        double lowest = grades.get(0);
+
+        for (double grade : grades) {
+            sum += grade;
+            if (grade > highest) {
+                highest = grade;
+            }
+            if (grade < lowest) {
+                lowest = grade;
+            }
+        }
+
+        double average = sum / grades.size();
+
+        System.out.println("Average grade: " + average);
+        System.out.println("Highest grade: " + highest);
+        System.out.println("Lowest grade: " + lowest);
+    }
+}
