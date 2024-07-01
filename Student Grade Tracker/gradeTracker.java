154abc58b49acc3a71fa3507869d7cfb092bf3bb
Sameer Mungase <143208446+sameermungase@users.noreply.github.com>
2024-07-02 01:03:17 +0530

    added new feature

--- Student Grade Tracker/gradeTracker.java
+++ Student Grade Tracker/gradeTracker.java
@@ -6,36 +6,48 @@ public class gradeTracker {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the number of students:");
-        int numStudents = scanner.nextInt();
-
-        ArrayList<Double> grades = new ArrayList<>();
+        System.out.print("Enter the number of subjects of student : ");
         
-        for (int i = 0; i < numStudents; i++) {
-            System.out.println("Enter the grades for student " + (i + 1) + ":");
-            double grade = scanner.nextDouble();
-            grades.add(grade);
+        ArrayList<Double> marks = new ArrayList<>();
+        int numSub = scanner.nextInt();
+        for (int i = 0; i < numSub; i++) {
+            System.out.println("Enter the grades for student in subject " + (i + 1) + ":");
+            double markspersub = scanner.nextDouble();
+            marks.add(markspersub);
         }
 
         double sum = 0;
-        double highest = grades.get(0);
-        double lowest = grades.get(0);
+        double highest = marks.get(0);
+        double lowest = marks.get(0);
 
-        for (double grade : grades) {
-            sum += grade;
-            if (grade > highest) {
-                highest = grade;
+        for (double totalM : marks) {
+            sum += totalM;
+            if (totalM > highest) {
+                highest = totalM;
             }
-            if (grade < lowest) {
-                lowest = grade;
+            if (totalM < lowest) {
+                lowest = totalM;
             }
         }
+        String grade = "";
+        int percentage = 0; 
+        if (percentage >=90){
+            grade ="A+";
+        }else if (percentage >=80) {
+            grade = "A";
+        }else if (percentage >=70) {
+            grade = "B+";
+        }else if (percentage >50) {
+            grade = "B";
+        }else if (percentage <=50) {
+            grade = "F";
+        }
 
-        double average = sum / grades.size();
-
+        double average = sum / marks.size();
+         percentage =(int)average  ;
         System.out.println("Average grade: " + average);
         System.out.println("Highest grade: " + highest);
         System.out.println("Lowest grade: " + lowest);
+        System.out.println("Student grade : "+ grade );
     }
 }
